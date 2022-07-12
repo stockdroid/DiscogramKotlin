d2369f062e0f38a82ae2b9d0e2b70c9d9a7d3bd6
Christian <chicchi7393@gmail.com>
2022-07-12 16:20:29 +0200

    Intercom works

--- src/main/kotlin/me/chicchi7393/discogramRewrite/telegram/UpdateHandler.kt
+++ src/main/kotlin/me/chicchi7393/discogramRewrite/telegram/UpdateHandler.kt
@@ -3,7 +3,11 @@ import it.tdlight.client.SimpleTelegramClient
 import it.tdlight.jni.TdApi.*
 import me.chicchi7393.discogramRewrite.JsonReader
 import me.chicchi7393.discogramRewrite.discord.DsApp
-import me.chicchi7393.discogramRewrite.mongoDB.databaseManager
+import me.chicchi7393.discogramRewrite.mongoDB.DatabaseManager
+import me.chicchi7393.discogramRewrite.objects.databaseObjects.ticketDocument
+import net.dv8tion.jda.api.Permission
+import net.dv8tion.jda.api.entities.TextChannel
+import java.util.*
 
 class UpdateHandler(private val tgClient: SimpleTelegramClient) {
 	private val settings = JsonReader().readJsonSettings("settings")!!
@@ -20,15 +24,50 @@ class UpdateHandler(private val tgClient: SimpleTelegramClient) {
     }
     fun onUpdateNewMessage(update: UpdateNewMessage) {
 		val messageContent = update.message.content
-		val dbMan = databaseManager.instance
-		val text: String = if (messageContent is MessageText) messageContent.text.text else String.format("(%s)", update.message)
+		val dbMan = DatabaseManager.instance
+		val text: String =
+			if (messageContent is MessageText) messageContent.text.text else String.format("(%s)", update.message)
 
 		tgClient.send(GetChat(update.message.chatId)) { chatIdResult ->
 			val dsClass = DsApp.instance
+			val message = update.message
 			val chat = chatIdResult.get()
-			if (chat.type is ChatTypePrivate && chat.id !in settings.discord["ignoreTGAuthor"] as List<Long>) {
-				dsClass.sendStartEmbed(chat, text, dbMan.utils().getLastUsedTicketId()+1)
-
+			if (chat.type is ChatTypePrivate && chat.id !in settings.discord["ignoreTGAuthor"] as List<Long> && (message.senderId as MessageSenderUser).userId != settings.telegram["userbotID"] as Long) {
+				if (dbMan.Utils().searchAlreadyOpen(chat.id) != null) {
+					dsClass.dsClient
+						.getChannelById(
+							TextChannel::class.java,
+							dbMan.Utils().searchAlreadyOpen(chat.id)!!.channelId
+						)!!
+						.sendMessage(text)
+						.queue()
+				} else {
+					dsClass.dsClient
+						.getCategoryById(
+							settings.discord["category_id"] as Long
+						)!!
+						.createTextChannel(
+							"${settings.discord["IDPrefix"]}${dbMan.Utils().getLastUsedTicketId() + 1}"
+						)
+						.map {
+							dbMan.Create().Tickets().createTicketDocument(
+								ticketDocument(
+									chat.id,
+									it.idLong,
+									dbMan.Utils().getLastUsedTicketId() + 1,
+									mapOf("open" to true, "suspended" to false, "closed" to false),
+									System.currentTimeMillis() / 1000
+								)
+							)
+							dsClass.sendStartEmbed(
+								chat,
+								text,
+								dbMan.Utils().getLastUsedTicketId() + 1,
+								"https://discordapp.com/channels/${dsClass.getGuild().idLong}/${it.idLong}"
+							)
+						}
+						.queue()
+				}
 			}
 		}
 	}
