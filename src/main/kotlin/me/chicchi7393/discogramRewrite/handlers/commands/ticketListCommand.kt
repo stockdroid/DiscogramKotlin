4e764b17c707093c0bc6edc2133fb7ba743f7aa7
Christian <chicchi7393@gmail.com>
2022-08-16 20:26:17 +0200

    Enhancements

--- src/main/kotlin/me/chicchi7393/discogramRewrite/handlers/commands/ticketListCommand.kt
+++ src/main/kotlin/me/chicchi7393/discogramRewrite/handlers/commands/ticketListCommand.kt
@@ -3,7 +3,6 @@ package me.chicchi7393.discogramRewrite.handlers.commands
 import me.chicchi7393.discogramRewrite.JsonReader
 import me.chicchi7393.discogramRewrite.discord.utils.getId
 import me.chicchi7393.discogramRewrite.mongoDB.DatabaseManager
-import me.chicchi7393.discogramRewrite.telegram.TgApp
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 
 class ticketListCommand(val event: SlashCommandInteractionEvent) {
