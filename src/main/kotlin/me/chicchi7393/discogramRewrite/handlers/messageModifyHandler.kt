6185fb42412d8c3285a36d7bde413f6a7462f092
Christian <chicchi7393@gmail.com>
2022-08-22 01:21:15 +0200

    Fixed a bug

--- src/main/kotlin/me/chicchi7393/discogramRewrite/handlers/messageModifyHandler.kt
+++ src/main/kotlin/me/chicchi7393/discogramRewrite/handlers/messageModifyHandler.kt
@@ -20,28 +20,51 @@ class messageModifyHandler(val event: GenericMessageEvent) {
     private val tgClient = TgApp.instance.client
     fun onMessageDelete(): Boolean {
         val newEvent = event as MessageDeleteEvent
-        val ticket: TicketDocument? = dbMan.Search().Tickets().searchTicketDocumentByChannelId(event.threadChannel.idLong)
-        if (ticket == null) {return false}
-        val tgId = dbMan.Search().MessageLinks().searchTgMessageByDiscordMessage(ticket.ticketId, newEvent.messageIdLong)
-        if (tgId == null) {return false}
-        tgClient.send(DeleteMessages(ticket.telegramId, longArrayOf(tgId), true)) {}
+        try {
+            val ticket: TicketDocument? = dbMan.Search().Tickets().searchTicketDocumentByChannelId(event.threadChannel.idLong)
+            if (ticket == null) {return false}
+            val tgId = dbMan.Search().MessageLinks().searchTgMessageByDiscordMessage(ticket.ticketId, newEvent.messageIdLong)
+            if (tgId == null) {return false}
+            tgClient.send(DeleteMessages(ticket.telegramId, longArrayOf(tgId), true)) {}
+        } catch (_: Exception) {return false}
         return true
     }
     fun onMessageUpdate(): Boolean {
         val newEvent = event as MessageUpdateEvent
-        val ticket: TicketDocument? = dbMan.Search().Tickets().searchTicketDocumentByChannelId(event.threadChannel.idLong)
-        if (ticket == null) {return false}
-        val tgId = dbMan.Search().MessageLinks().searchTgMessageByDiscordMessage(ticket.ticketId, newEvent.messageIdLong)
-        if (tgId == null) {return false}
-        tgClient.send(GetMessage(ticket.telegramId, tgId)) {
-            if (it.get().content is MessageText) {
-                tgClient.send(EditMessageText(ticket.telegramId, tgId, null, InputMessageText(TdApi.FormattedText(newEvent.message.contentRaw, null), false, false))) {}
-            } else {
-                tgClient.send(EditMessageCaption(ticket.telegramId, tgId, null, TdApi.FormattedText(newEvent.message.contentRaw, null))) {}
+        try {
+            val ticket: TicketDocument? =
+                dbMan.Search().Tickets().searchTicketDocumentByChannelId(event.threadChannel.idLong)
+            if (ticket == null) {
+                return false
             }
+            val tgId =
+                dbMan.Search().MessageLinks().searchTgMessageByDiscordMessage(ticket.ticketId, newEvent.messageIdLong)
+            if (tgId == null) {
+                return false
+            }
+            tgClient.send(GetMessage(ticket.telegramId, tgId)) {
+                if (it.get().content is MessageText) {
+                    tgClient.send(
+                        EditMessageText(
+                            ticket.telegramId,
+                            tgId,
+                            null,
+                            InputMessageText(TdApi.FormattedText(newEvent.message.contentRaw, null), false, false)
+                        )
+                    ) {}
+                } else {
+                    tgClient.send(
+                        EditMessageCaption(
+                            ticket.telegramId,
+                            tgId,
+                            null,
+                            TdApi.FormattedText(newEvent.message.contentRaw, null)
+                        )
+                    ) {}
+                }
 
-        }
-
+            }
+        } catch (_: Exception) {return false}
         return true
     }
 }
