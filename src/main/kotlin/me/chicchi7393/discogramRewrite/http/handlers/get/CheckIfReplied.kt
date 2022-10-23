ed253dad121db2f6c215871766b7d522e69fcb90
Chicchi7393 <25307474+Chicchi7393@users.noreply.github.com>
2022-10-23 22:46:15 +0200

    Fixed some obvious mistakes

--- src/main/kotlin/me/chicchi7393/discogramRewrite/http/handlers/get/CheckIfReplied.kt
+++ src/main/kotlin/me/chicchi7393/discogramRewrite/http/handlers/get/CheckIfReplied.kt
@@ -11,17 +11,17 @@ class CheckIfReplied : HTTPHandlerClass() {
     override fun handle(ctx: Context): Context {
         try {
             val args = ConvertQueryStringToMap.convert(ctx.req.queryString)
-            val time: Int = if (!args.containsKey("time")) 86400 else args["time"]!!
-            if (ctx.body().isEmpty() or !ctx.body().all { char -> char.isDigit() }) return ctx.status(406)
-            val ticket = dbMan.Search().Tickets().searchTicketDocumentsByTelegramId(ctx.body().toLong())
-            if (ticket.isEmpty()) return ctx.status(406)
-            if (ticket[0]!!.unixSeconds >= (System.currentTimeMillis() / 1000) - time) {
-                return ctx.json(mapOf("result" to true))
-            } else {
-                return ctx.json(mapOf("result" to false))
-            }
+            val time: Int = args["time"]!!
         } catch (_: NullPointerException) {
-            return ctx.status(404).result("No args specified")
+            val time: Int = 86400
+        }
+        if (ctx.body().isEmpty() or !ctx.body().all { char -> char.isDigit() }) return ctx.status(406)
+        val ticket = dbMan.Search().Tickets().searchTicketDocumentsByTelegramId(ctx.body().toLong())
+        if (ticket.isEmpty()) return return ctx.json(mapOf("result" to false))
+        if (ticket[0]!!.unixSeconds >= (System.currentTimeMillis() / 1000) - time) {
+            return ctx.json(mapOf("result" to true))
+        } else {
+            return ctx.json(mapOf("result" to false))
         }
     }
 }
